d720302a381da15169303d412c950623e804d123
Evelin <evelin.foxell@purplescout.se>
2017-02-04 20:55:12 +0100

    Add a C version before I learn anything more than Hello World to track my progress.

---
+++ c/version_one.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+/*
+ * Let's have a go at writing a FizzBuzz before I Know anything 
+ * about C except for how to print "Hello World!". 
+ */
+int main(void)
+{
+	puts("Let's play fizzbuzz!\n");
+	
+	int i;
+	for(i = 1; i <=100; i++)
+	{
+		if(i % 3 == 0 || i % 5 == 0) 
+		{
+			if(i % 3 == 0 && i % 5 != 0) { puts("Fizz"); }
+			else if(i % 5 == 0 && i % 3 != 0) { puts("Buzz"); }
+			else { puts("FizzBuzz"); } 
+		} else 
+		{ 
+			printf("%d\n", i);
+		}
+	}
+
+	return(0);
+}
